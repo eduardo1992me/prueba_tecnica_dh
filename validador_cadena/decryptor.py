e98eb3fb517cdb06d7201893047b897896eeaf0a
Eduardo Medina <eduardo1992me@gmail.com>
2023-10-07 23:59:10 -0600

    code without comments and print´s

--- validador_cadena/decryptor.py
+++ validador_cadena/decryptor.py
@@ -1,5 +1,4 @@
 def reader(data):
-    #cadena_test = "Robert0000000Smith000123"
     control = 0
     name = ""
     last_name = ""
@@ -9,13 +8,9 @@ def reader(data):
     sub_pos_2 = []
 
     for i in range(len(data)):
-        print(data[i])
         if data[i] == "0":
             pos.append(i)
-    print(pos)
     for a in range(len(pos)):
-        print(f"El for va en{pos[a]}")
-        print(control)
         try:
             if pos[a] == pos[a + 1] - 1 and control == 0:
                 sub_pos_1.append(pos[a])
@@ -33,14 +28,9 @@ def reader(data):
         except:
             pass
 
-    print(f"Sub_pos_1: {sub_pos_1} y Sub_pos_2: {sub_pos_2}")
-
-    # "Robert000Smith000123"
     control = 0
     try:
         for b in data:
-            print(b)
-            print(sub_pos_1[-1], sub_pos_2[0])
             if control < sub_pos_1[0]:
                 name += b
             if control > sub_pos_1[-1] and control < sub_pos_2[0]:
@@ -50,9 +40,6 @@ def reader(data):
             control += 1
     except:
         pass
-    print(name)
-    print(last_name)
-    print(id)
     decrypted = {}
     decrypted = {"first_name" : name, "last_name": last_name, "id": id}
     return decrypted
